3816d2302124168415497a50e0b6f67ea2d1f8b9
teemu <teemu@valhalla.(none)>
2012-04-04 16:47:56 +0300

    Added sequence candidate generation.

--- py/algorithms.py
+++ py/algorithms.py
@@ -218,3 +218,54 @@ def ap_genrules(frequent_itemsets, rules, minConfidence, f, H):
 	if k > m+1:
 		candidates = generate_candidates(H, m)
 		ap_genrules(frequent_itemsets, rules, minConfidence, f, candidates)
+
+def exclude_first_event(seq):
+	new_first_elem = seq[0][1:]
+	if new_first_elem: return (new_first_elem,) + seq[1:]
+	return seq[1:]
+
+def exclude_last_event(seq):
+	new_last_elem = seq[-1][:-1]
+	if new_last_elem: return seq[:-1] + (new_last_elem,)
+	return seq[:-1]
+
+def all_pairs(seqs):
+	for s in seqs:
+		for t in seqs:
+			yield (s,t)
+
+def seq_candidate_generation(sequences, k):
+	"""Candidate sequence generation.
+	Parameters:
+		sequences - (k-1)-sequences
+		k - a positive integer, k > 1
+	"""
+	candidates = []
+	if k == 2:
+		for s,t in itertools.combinations(sequences,2):
+			e1 = s[0]
+			e2 = t[0]
+			candidates.append( ((e1[0],e2[0]),) )
+		for s,t in all_pairs(sequences):
+			candidates.append( (s[0],t[0]) )
+		candidates.sort()
+		return candidates
+	for s,t in all_pairs(sequences):
+		if exclude_first_event(s) == exclude_last_event(t):
+			event = t[-1][-1] # last event in the last element of t
+			if len(t[-1]) == 1:
+				c = s + ((event,),)  
+			else:
+				c = s[:-1] + (s[-1] + (event,),)
+			# check for all contiguous (k-1)-subsequences and prune if one of them is infreq.
+			infreq = False
+			for e in xrange(len(c)):
+				for i in xrange(len(c[e])):
+					elem = c[e][:i] + c[e][i+1:]
+					if elem: elem = (elem,)
+					subseq = c[:e] + elem + c[e+1:]
+					if subseq not in sequences:
+						infreq = True
+						break
+			if not infreq: candidates.append(c)
+	return candidates
